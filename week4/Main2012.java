b3b13cf2e4fbef7be9342cd2c3e9d6fba111a123
hellozo0 <ahju0011@gmail.com>
2024-01-30 18:27:11 +0900

    [백준] n.2012 - 성공

--- week4/Main2012.java
+++ week4/Main2012.java
@@ -9,19 +9,20 @@ public class Main2012 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        long sum = 0;
 
         int[] score = new int[N];
         for(int i = 0; i < N; i++){
             score[i] = Integer.parseInt(br.readLine());
-            sum += score[i];
         }
 
         Arrays.sort(score);
+        long sum = 0;
 
-        long maxSum = score[N-1] * (score[N-1] + 1) / 2;
+        for(int i = 0; i < N; i++) {
+            sum += Math.abs(score[i] - (i + 1));
+        }
 
-        System.out.println(maxSum-sum);
+        System.out.println(sum);
 
     }
 }
