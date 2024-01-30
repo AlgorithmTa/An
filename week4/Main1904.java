eccca52f286603fa4a1ffaffb2637f1cf4d773f7
hellozo0 <ahju0011@gmail.com>
2024-01-30 20:08:03 +0900

    [백준] n.2904 - 실패(런타임에러)

---
+++ week4/Main1904.java
@@ -0,0 +1,24 @@
+package week4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main1904 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] tiles = new int[N+1];
+        tiles[0] = 0;
+        tiles[1] = 1;
+        tiles[2] = 2;
+
+        for(int i = 3; i <= N; i++){
+            tiles[i]  = (tiles[i-1] + tiles[i-2]) % 15746;
+        }
+
+        System.out.println(tiles[N]);
+    }
+}
